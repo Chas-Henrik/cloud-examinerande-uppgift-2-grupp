d6d0f0d988b232aac9de9f3902c33a2754210740
Chas-Henrik <henrik.suurik@chasacademy.se>
2025-11-03 16:07:35 +0100

    17 lighthouse ci (#35)
    * Added Lighthouse CI
    
    * Updated GitHub workflow
    
    * Updated github workflow
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated lighthouserc.js
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated lighthouse config
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated Lighthouse config
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated Lighthouse config
    
    * Run lint-and-test & lighthouseci in parallell
    
    * Only run Lighthouse on PR's
    
    * Use different URL for development
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated GitHub workflow
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Updated Lighthouse CI
    
    * Fixed invalid syntax in lighthouse.yml file
    
    * Updated Lighthouse CI
    
    * Tried to fix error in yaml syntax on line 55 in lighthouse.yml
    
    * Tried to fix error in yaml syntax on line 55 in lighthouse.yml
    
    * Tried to fix error in yaml syntax on line 55 in lighthouse.yml
    
    * Tried to fix error in yaml syntax on line 55 in lighthouse.yml
    
    * Just a test
    
    * Tried to fix error in yaml syntax on line 55 in lighthouse.yml
    
    * Tried to fix error in yaml syntax on line 55 in lighthouse.yml
    
    * Tried to fix error in yaml syntax on line 55 in lighthouse.yml
    
    * Just a test
    
    * Tried to fix error in yaml syntax on line 55 in lighthouse.yml
    
    * Updated Lighthouse CI
    
    * Added test step to verify key
    
    * Removed test
    
    * Added test
    
    * Updated test
    
    * Removed test
    
    * Removed VERCEL_AUTOMATION_BYPASS_SECRET
    
    * Swapped order on mobile & desktop audit
    
    * Added mobile configuration
    
    * Separated build contexts for desktop & mobile
    
    * Added LHCI_BUILD_CONTEXT__CURRENT_REF for mobile & desktop
    
    * Attempted to separate desktop & mobile reports
    
    * Revert "Attempted to separate desktop & mobile reports"
    
    This reverts commit e249ce5f705fab52dac3b6287135220b45059f47.
    
    * Attempted to separate desktop & mobile reports
    
    * Removed LHCI_COLLECT__SETTINGS__PSI_STRATEGY from env
    
    * Minor cleanup
    
    * PREVIEW_URL test
    
    * PREVIEW_URL test
    
    * Removed PREVIEW_URL test
    
    * Test changes
    
    * Removed .lighthouseci folder
    
    * Removed invisible character
    
    ---------
    
    Co-authored-by: Viktor <vigus006@gmail.com>

---
+++ .lighthouserc.desktop.js
@@ -0,0 +1,35 @@
+// .lighthouserc.desktop.js
+
+module.exports = {
+  ci: {
+    collect: {
+      url: process.env.NODE_ENV === 'production' ? [process.env.LHCI_URL] : ['http://localhost:3000/login'],
+      startServerCommand: "npm run build && npm run start", // optional, if you need a dev server
+      numberOfRuns: 3,
+      settings: {
+        preset: "desktop"
+      },
+      static: false, // crucial for live SSR apps
+    },
+    assert: {
+      preset: process.env.NODE_ENV === 'production' ? "lighthouse:no-pwa" : "lighthouse:recommended",
+      assertions: {
+        // Critical issues that will fail CI
+        "errors-in-console": "fail",
+        "accessibility": "fail", // optional if you want to enforce accessibility
+
+        // Warnings for non-critical issues
+        "legacy-javascript": "warn",
+        "legacy-javascript-insight": "warn",
+        "network-dependency-tree-insight": "warn",
+        "render-blocking-resources": "warn",
+        "unused-javascript": "warn",
+        "redirects": "warn",
+        "document-latency-insight": "warn",
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
